f5bba5fdeff8f554300d0786ac717b4229b7e990
LahiaOmar <omar.lahia@gmail.com>
2020-03-06 15:22:39 +0100

    add some modification to select html tag

--- popup.js
+++ popup.js
@@ -1,25 +1,32 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
     var themesSelectElement = document.getElementById("themes");
-    themesSelectElement.addEventListener("change", function() {
+    var selectedTheme = localStorage.getItem("theme")
+    if (selectedTheme != null) {
+      for (var i = 0; i < themesSelectElement.length; i++) {
+        if (themesSelectElement[i].value == selectedTheme) {
+          themesSelectElement[i].selected = true;
+        }
+      }
+    }
+
+    themesSelectElement.addEventListener("change", function (params) {
       var themeName = this.value;
     });
 
-
     var applyButtonElement = document.getElementsByName("apply")[0];
-    applyButtonElement.addEventListener("click", function() {
-
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    applyButtonElement.addEventListener("click", function () {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var themesSelectElement = document.getElementById("themes");
         var themeName = themesSelectElement.value;
-
+        localStorage.setItem("theme", themeName)
         var message = {
           activate: true,
           theme: themeName
         };
 
-        chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
-          console.log(response);
+        chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
+          console.log(response)
         });
       });
 
@@ -28,8 +35,8 @@
 
 
     var restoreButtonElement = document.getElementsByName("restore")[0];
-    restoreButtonElement.addEventListener("click", function() {
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    restoreButtonElement.addEventListener("click", function () {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var themesSelectElement = document.getElementById("themes");
         var themeName = themesSelectElement.value;
 
@@ -37,12 +44,12 @@
           activate: false
         };
 
-        chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
-          console.log(response);
+        chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
+          console.log(response)
         });
       });
 
       window.close();
     });
   });
-})();
+})()
